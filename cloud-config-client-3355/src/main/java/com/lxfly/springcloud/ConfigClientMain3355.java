1e40c3071b679645f21bc145f3e063137d951683
lxfly <978354837@qq.com>
2020-11-30 00:15:13 +0800

    spring-config/bus消息总线结合 动态广播/定点刷新 无需重启

---
+++ cloud-config-client-3355/src/main/java/com/lxfly/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,15 @@
+package com.lxfly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxfly
+ * @create 2020-11-29 22:52
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
